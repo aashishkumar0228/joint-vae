cb02c3ce1d951da2f87aa6f1bbca9f12decd6b22
Aashish Kumar <aashish.kumar1@byjus.com>
2021-05-11 14:42:18 +0530

    changes

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ img_size = (1, 32, 32)
 
 # Define latent spec and model
 # latent_spec = {'cont': 10, 'disc': [10]}
-latent_spec = {'cont': 20}
+latent_spec = {'cont': 10}
 hidden_dim = 256
 model = VAE(img_size=img_size, latent_spec=latent_spec,
             use_cuda=use_cuda, hidden_dim=hidden_dim)
@@ -42,8 +42,8 @@ optimizer = optim.Adam(model.parameters(), lr=lr)
 
 # Define trainer
 trainer = Trainer(model, optimizer,
-                  cont_capacity=[0.0, 50.0, 25000, 100.0],
-                  disc_capacity=[0.0, 50.0, 25000, 100.0],
+                  cont_capacity=[0.0, 50.0, 25000, 50.0],
+                  disc_capacity=[0.0, 50.0, 25000, 50.0],
                   use_cuda=use_cuda)
 
 # Train model for 100 epochs
